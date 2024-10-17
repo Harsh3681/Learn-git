5fa82f32b0527ec8ca20aca9f7da08a462768ec4
harshal <harshal.sonawane21@pccoepune.org>
2024-10-17 11:37:52 +0530

    new deque file

--- counter.php
+++ counter.php
@@ -7,20 +7,18 @@
 </head>
 <body>
     
-
-
-<?php 
-  
-  // PHP program to illustrate working of count() 
-  $array = array('names' => array('Aakash', 'Ravi', 'Prashant'), 
-                 'rollno' => array('5', '10', '15')); 
+    <?php 
     
-  // recursive count - mode as 1 
-  echo("Recursive count: ".count($array,1)."\n"); 
-    
-  // normal count - mode as 0 
-  echo("Normal count: ".count($array,0)."\n"); 
-    
-  ?> 
+    // PHP program to illustrate working of count() 
+    $array = array('names' => array('Aakash', 'Ravi', 'Prashant'), 
+                    'rollno' => array('5', '10', '15')); 
+        
+    // recursive count - mode as 1 
+    echo("Recursive count: ".count($array,1)."\n"); 
+        
+    // normal count - mode as 0 
+    echo("Normal count: ".count($array,0)."\n"); 
+        
+    ?> 
 </body>
 </html>
